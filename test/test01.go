386c2e2e7b24a4d5d34753585be03baa471e774d
Jason <>
2022-03-25 18:13:57 -0400

    organize gomarc21 pkg

--- test/test01.go
+++ test/test01.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
-  "github.com/jasonzou/gomarc21/pkg/marc"
+  "github.com/jasonzou/gomarc21"
 )
 
 func main() {
-	tag, err := NewTagByStr("123")
+	tag, err := gomarc21.NewTagByStr("123")
+  if err != nil {
+    fmt.Print(err)
+  }
 	fmt.Print(tag)
 }
